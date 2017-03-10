85d86a6373dd30d4bb2ab10cd5ba17cf64666a11
Kazuki Shimizu <kazuki43zoo@gmail.com>
2017-03-11 00:50:20 +0900

    Replace to @Import(DemoRepository.class)

--- gh-8566/src/test/java/com/example/repository/DemoRepositoryTests.java
+++ gh-8566/src/test/java/com/example/repository/DemoRepositoryTests.java
@@ -5,12 +5,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @JdbcTest
-@ComponentScan
+@Import(DemoRepository.class)
 public class DemoRepositoryTests {
 
     @Autowired
